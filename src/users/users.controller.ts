6708c9329449ede1f5bdf5f3c6a58c925c06a39c
Ivan <fistashkin0123@gmail.com>
2023-08-21 15:49:50 +0300

    add: sign jwt func

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,14 +8,17 @@ import 'reflect-metadata';
 import { IUserController } from './users.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
-import { UserService } from './users.service';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../config/config.service.interface';
+import { IUserService } from './users.service.interface';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.ILogger) private loggerService: ILogger,
-		@inject(TYPES.UserService) private userService: UserService,
+		@inject(TYPES.UserService) private userService: IUserService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -34,16 +37,17 @@ export class UserController extends BaseController implements IUserController {
 		res: Response,
 		next: NextFunction,
 	): Promise<void> {
-		const userDto = req.body;
-		const loggedIn = await this.userService.validateUser(userDto);
+		const loggedIn = await this.userService.validateUser(req.body);
 		if (!loggedIn) {
 			return next(new HTTPError(401, 'auth error', 'login'));
 		}
 
+		const token = await this.signJWT(req.body.email, this.configService.get('SECRET'));
+
 		this.ok<{
-			loggedIn: boolean;
+			token: string;
 		}>(res, {
-			loggedIn,
+			token,
 		});
 	}
 
@@ -58,5 +62,27 @@ export class UserController extends BaseController implements IUserController {
 		}
 
 		this.ok(res, { email: result.email, id: result.id });
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{
+					algorithm: 'HS256',
+				},
+				(err, token) => {
+					if (err) {
+						reject(err);
+					}
+
+					resolve(token as string);
+				},
+			);
+		});
 	}
 }
